c46713ee3dcd37f5a88888b8ea19a31694a360e4
🎃 Christoffer 🎃 <strand.arnesen@gmail.com>
2021-05-03 12:02:56 +0200

    login metoder

--- Klient/src/klient/Klient.java
+++ Klient/src/klient/Klient.java
@@ -5,12 +5,17 @@
  */
 package klient;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -20,13 +25,20 @@ import javafx.stage.Stage;
 
 
 public class Klient extends Application {
-    BorderPane bpane;
-    Pane top = new Pane();
-    Pane center = new Pane();
-    Pane bottom = new Pane();
-    Label topLabel = new Label("Tast inn brukernavn: ");
-    TextField txt = new TextField();
-    Button btn = new Button("Log inn");
+    static BorderPane bpane;
+    //Login deklarasjoner
+    static Pane topLogin = new Pane();
+    static Pane centerLogin = new Pane();
+    static Pane bottomLogin = new Pane();
+    static Label topLabelLogin = new Label("Tast inn brukernavn: ");
+    static TextField txtLogin = new TextField();
+    static Button btnLogin = new Button("Log inn");
+    
+    //Rom deklarasjoner
+    static Pane topRom = new Pane();
+    static Pane centerRom = new Pane();
+    static Label labelRom = new Label("Chat-Rom: ");
+    static ListView list = new ListView();
     
     
     
@@ -34,30 +46,41 @@ public class Klient extends Application {
     public void start(Stage primaryStage) {
     
         bpane = new BorderPane();
-        bpane.setTop(top);
-        bpane.setCenter(center);
-        bpane.setBottom(bottom);
+        /* Login side panes */
+        bpane.setTop(topLogin);
+        bpane.setCenter(centerLogin);
+        bpane.setBottom(bottomLogin);
         
-        top.setPrefHeight(100);
-        top.setStyle("-fx-border-color: black; -fx-background-color: grey;");
-        center.setPrefHeight(300);
-        center.setStyle("-fx-border-color: black; -fx-background-color: white;");
-        bottom.setPrefHeight(100);
-        bottom.setStyle("-fx-border-color: black; -fx-background-color: grey;");
-        topLabel.setStyle("-fx-text-fill:BLACK; -fx-font-size: 30;");
+        topLogin.setPrefHeight(100);
+        topLogin.setStyle("-fx-border-color: black; -fx-background-color: grey;");
+        centerLogin.setPrefHeight(300);
+        centerLogin.setStyle("-fx-border-color: black; -fx-background-color: white;");
+        bottomLogin.setPrefHeight(100);
+        bottomLogin.setStyle("-fx-border-color: black; -fx-background-color: grey;");
+        topLabelLogin.setStyle("-fx-text-fill:BLACK; -fx-font-size: 30;");
         
-        top.getChildren().add(topLabel);
-        topLabel.setLayoutX(25);
-        topLabel.setLayoutY(25);
+        topLogin.getChildren().add(topLabelLogin);
+        topLabelLogin.setLayoutX(25);
+        topLabelLogin.setLayoutY(25);
         
-        center.getChildren().add(txt);
-        txt.setLayoutX(150);
-        txt.setLayoutY(140);
-        center.getChildren().add(btn);
-        btn.setLayoutX(350);
-        btn.setLayoutY(140);
+        centerLogin.getChildren().add(txtLogin);
+        txtLogin.setLayoutX(150);
+        txtLogin.setLayoutY(140);
+        centerLogin.getChildren().add(btnLogin);
+        btnLogin.setLayoutX(350);
+        btnLogin.setLayoutY(140);
+        /* Slutt på Login panes */
         
-       
+        /*  Rom side panes */
+        topRom.setPrefHeight(50);
+        topRom.setStyle("-fx-border-color: black; -fx-background-color: grey;");
+        centerRom.setPrefHeight(450);
+        centerRom.setStyle("-fx-border-color: black; -fx-background-color: white;");
+        labelRom.setStyle("-fx-text-fill:BLACK; -fx-font-size: 30;");
+        
+        labelRom.setLayoutX(25);
+        labelRom.setLayoutY(7);
+        
         
         Scene scene = new Scene(bpane, 600, 500);
         
@@ -65,11 +88,26 @@ public class Klient extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    
+    public static void logInn() {
+    btnLogin.setOnAction((event) -> {
+        bpane.getChildren().remove(centerLogin);
+        bpane.getChildren().remove(topLogin);
+        bpane.getChildren().remove(bottomLogin);
+        
+        bpane.setTop(topRom);
+        bpane.setCenter(centerRom);
+        topRom.getChildren().add(labelRom);
+    
+    });
+        
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        logInn();
         launch(args);
     }
     
